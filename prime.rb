a610a799d85a7338cb7b44948fc3f41ecaf75ab3
Shivani Mathur <qwellery@gmail.com>
2017-12-29 06:55:14 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,12 @@
 def prime?(num)
   i = 2
-  isprime = false
+  isprime = true
   if num < 2
     return false
   end
   while i < num
     if (num % i == 0)
-      isprime = true
+      isprime = false
     end
     i += 1
   end
