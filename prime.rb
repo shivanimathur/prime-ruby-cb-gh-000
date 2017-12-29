e29437938ebe5b5d3edfd333ee0aba745ece30c6
Shivani Mathur <qwellery@gmail.com>
2017-12-29 06:55:23 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(num)
+  i = 2
+  isprime = true
+  if num < 2
+    return false
+  end
+  while i < num
+    if (num % i == 0)
+      isprime = false
+    end
+    i += 1
+  end
+  isprime
+end
