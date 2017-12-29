9ca583a6d278b6f58a7c4cd5398995c6477d2021
Shivani Mathur <qwellery@gmail.com>
2017-12-29 06:48:50 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,10 @@
 def prime?(num)
   i = 2
   while i < num &&
-    if (num % i == 0 || num < 0)
+    if num % i == 0
       return false
-
+    elsif num < 0
+      return false      
     else
       return true
     end
